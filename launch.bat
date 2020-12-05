29da27c408fae99504baaa41d8e07cbc41ab46a8
Jakkins <Jakkins@hotmail.it>
2020-12-05 03:05:36 +0100

    gradle doesn't work

---
+++ launch.bat
@@ -0,0 +1,15 @@
+rem https://ss64.com/nt/start.html
+rem https://ss64.com/nt/cmd.html
+rem JAVA_HOME -> JDK
+rem GRADLE_HOME -> why?
+rem npx --stacktrace react-native run-android
+
+cd C:\Dev\Github\Todos
+cd .\backend\
+start cmd.exe "/k node server.js"
+
+cd ..
+cd .\frontend\
+start cmd.exe "/k npx react-native run-android --verbose"
+
+cd ..
