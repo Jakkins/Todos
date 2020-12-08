2480b46043b5f5dde7fc8d0ba1bb7c27b32c9a4c
Jakkins <Jakkins@hotmail.it>
2020-12-08 03:14:02 +0100

    gradle wrapper version changed

--- launch.bat
+++ launch.bat
@@ -1,8 +1,16 @@
 rem https://ss64.com/nt/start.html
 rem https://ss64.com/nt/cmd.html
-rem JAVA_HOME - JDK
-rem GRADLE_HOME - why?
+
+rem SDK Manager, SDK Tool, Install 
+rem JAVA_HOME - jdk-8u271
+rem GRADLE_HOME
+rem GRADLE_USER_HOME
+
 rem npx --stacktrace react-native run-android
+
+rem emulator -list-avds
+rem npm run-script android
+rem cd android && gradlew.bat app:installDebug -PreactNativeDevServerPort=8081 --status
 
 cd C:\Dev\Github\Todos
 cd .\backend\
